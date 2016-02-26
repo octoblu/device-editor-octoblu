d855d905eb49e37d8402b2f7c27f0051ebb833de
ASteinheiser <asteinhe@asu.edu>
2016-02-26 14:05:09 -0700

    added search page

---
+++ src/containers/search-device.js
@@ -0,0 +1,53 @@
+import React, { Component, PropTypes } from 'react'
+
+export default class SearchDevice extends Component {
+
+  state = {
+    uuid: '',
+    token: ''
+  }
+
+  handleSubmit = () => {
+    window.location = '/' + this.state.uuid + '/' + this.state.token + '/schema'
+  }
+
+  handleChange = (event) => {
+    if (event.target.name == 'uuid') {
+      this.setState({uuid: event.target.value})
+
+    } else if (event.target.name == 'token') {
+      this.setState({token: event.target.value})
+    }
+  }
+
+  render() {
+    return <div>
+      <h2>Search for Device</h2>
+
+      <form name='search'>
+        <font>Enter your device's uuid: </font>
+        <input
+          name='uuid'
+          onChange={this.handleChange}
+        />
+        <br/>
+
+        <font>Enter your device's token: </font>
+        <input
+          name='token'
+          onChange={this.handleChange}
+        />
+        <br/>
+
+        <button
+          name='searchDevice'
+          form='search'
+          type='submit'
+          onClick={this.handleSubmit}>
+          Search for device
+        </button>
+      </form>
+
+    </div>
+  }
+}
--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ import { Router, Route, browserHistory } from 'react-router'
 
 import Layout from './containers/layout'
 import SchemaEditor from './containers/schema-editor'
+import SearchDevice from './containers/search-device'
 
 render((
   <Router history={browserHistory}>
     <Route path="/" component={Layout}>
+      <Route path="/search" component={SearchDevice}/>
       <Route path="/:uuid/:token/schema" component={SchemaEditor}/>
     </Route>
   </Router>
