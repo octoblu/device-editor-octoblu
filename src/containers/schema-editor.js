919944650511446fe2536156487a3706781d5bec
ASteinheiser <asteinhe@asu.edu>
2016-03-02 13:03:51 -0700

    removed unused packages, replaced querystring with router, updated zooid-editor

--- src/containers/schema-editor.js
+++ src/containers/schema-editor.js
@@ -1,32 +1,29 @@
-import _ from 'lodash'
 import React, { Component, PropTypes } from 'react'
-import ReactSchemaForm from 'react-jsonschema-form'
-import querystring from 'querystring'
+
 import ZooidMeshbluDeviceEditor from 'zooid-meshblu-device-editor'
 
 export default class SchemaEditor extends Component {
   getMeshbluJson = () =>  {
     const { uuid } = this.props.params
-    var query = querystring.parse(location.search.substring(1, location.search.length))
+    var { token, server, port } = this.props.location.query;
+
     return {
       uuid,
-      token: query.token,
-      server: query.server || 'meshblu.octoblu.com',
-      port: query.port || 443
+      token,
+      server: server || 'meshblu.octoblu.com',
+      port: port || 443
     }
   }
 
   render() {
-    const { uuid, token, server, port } = this.getMeshbluJson()
-    
+    const meshbluConfig = this.getMeshbluJson()
+
     return <div>
       <h2>Schema Editor</h2>
 
       <ZooidMeshbluDeviceEditor
-        uuid={uuid}
-        token={token}
-        server={server}
-        port={port}
+        uuid={meshbluConfig.uuid}
+        meshbluConfig={meshbluConfig}
       />
     </div>
   }
