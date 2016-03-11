abe8b76b3d3b21aed8a239621b47f6c600682cd4
Square Root of Saturn <sqrt@octoblu.com>
2016-03-10 17:32:56 -0700

    v2.0.1 works with new device editor

--- src/containers/schema-editor.js
+++ src/containers/schema-editor.js
@@ -1,10 +1,13 @@
 import React, {PropTypes} from 'react'
-
 import MeshbluDeviceEditor from 'zooid-meshblu-device-editor'
 import MeshbluHttp from 'meshblu-http'
+import _ from 'lodash'
 
 export default React.createClass({
-  state: {},
+  getInitialState: function(){
+    return {}
+  },
+
   componentDidMount: function () {
     var {uuid} = this.props.params
     if(!this.props.params) return window.location = '/search'
@@ -27,26 +30,18 @@ export default React.createClass({
       return new MeshbluHttp(meshbluConfig)
   },
 
-  handleChange: function (device) {
+  handleChange: function ({name, options}) {
+    const device = _.extend({}, this.state.device, {name, options})
     this.setState({device})
-  },
-
-  handleSave: function () {
-    var {uuid, meshblu} = this
-    meshblu.update(uuid, (error, device)=> {
-      console.log(error, device)
-      this.setState({device})
+    this.meshblu.updateDangerously(this.uuid, {$set: {name, options}}, (error)=>{
+      if(error) console.log('Error!', error)
     })
   },
 
   render: function () {
+    if(!this.state) return
     const {device} = this.state
     if(!device) return <h3>Loading</h3>
-    return (
-      <div>
-        <MeshbluDeviceEditor device={device} onChange={this.handleChange} />
-      <button onClick={this.handleSave}>Save</button>
-      </div>
-    )
+    return (<MeshbluDeviceEditor device={device} onChange={this.handleChange} />)
   }
 })
